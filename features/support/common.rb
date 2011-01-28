59089482438707aa04c3b4451791c3316f0dca8c
Alister Scott <alister.scott@gmail.com>
2011-01-28 21:29:33 +1000

    Removed old pages

--- features/support/common.rb
+++
@@ -1,9 +0,0 @@
-module Common
-  def Common.get_search_term_data term
-    YAML.load_file("#{TEST_DATA_DIR}/search_test_data.yml")["search terms"][term]["search term"]
-  end
-
-  def Common.get_search_term_exp_res term
-    YAML.load_file("#{TEST_DATA_DIR}/search_test_data.yml")["search terms"][term]["expected results"]
-  end
-end
