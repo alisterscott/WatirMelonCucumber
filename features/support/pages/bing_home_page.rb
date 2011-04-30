edd5f1fc6518d530a58bd44f5553bce42df0a351
Alister Scott <alister.scott@gmail.com>
2011-04-30 21:57:26 +1000

    Minor change to bing and google home pages

--- features/support/pages/bing_home_page.rb
+++ features/support/pages/bing_home_page.rb
@@ -3,7 +3,6 @@ class BingHomePage < SearchPageClass
   direct_url "http://www.bing.com"
   expected_title "Bing"
 
-  #text_field :search, :name => "q"
   button :bing_search, :name => "go"
 
   def search_for term
--- features/support/pages/google_home_page.rb
+++ features/support/pages/google_home_page.rb
@@ -3,7 +3,6 @@ class GoogleHomePage < SearchPageClass
   direct_url "http://www.google.com"
   expected_title "Google"
 
-  #text_field :search, :name => "q"
   button :google_search, :name => "btnG"
 
   def search_for term
