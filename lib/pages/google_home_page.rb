8b9c97ac2a516126148a445b6129566b2c14864d
Alister Scott <alister.scott@gmail.com>
2011-09-05 21:58:32 +1000

    added go to google in english

--- lib/pages/google_home_page.rb
+++ lib/pages/google_home_page.rb
@@ -3,9 +3,12 @@ class GoogleHomePage < SearchPageClass
   direct_url "http://www.google.com"
   expected_title "Google"
   link :go_to_google_usa, :text => 'Go to Google.com'
+  link :go_to_google_in_english, :text => 'Google.com in English'
 
   def search_for term
     go_to_google_usa if go_to_google_usa_link.exists?
+    go_to_google_in_english if go_to_google_in_english_link.exists?
+
     self.search = term
     self.search_text_field.send_keys :enter
     GoogleResultsPage.new(@browser, false)
