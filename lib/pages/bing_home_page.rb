004b1529acf0fd806d1428ffb6e4116eb5a65f71
Alister Scott <alister.scott@gmail.com>
2011-09-05 22:04:22 +1000

    Fixed bing in German

--- lib/pages/bing_home_page.rb
+++ lib/pages/bing_home_page.rb
@@ -3,9 +3,12 @@ class BingHomePage < SearchPageClass
   direct_url "http://www.bing.com"
   expected_title "Bing"
 
-  button :bing_search, :name => "go"
+  button :bing_search, :name => 'go'
+  link :switch_to_english, :text => 'English'
 
   def search_for term
+    switch_to_english if switch_to_english_link.exists?
+
     self.search = term
     self.search_text_field.send_keys :enter
     BingResultsPage.new(@browser)
