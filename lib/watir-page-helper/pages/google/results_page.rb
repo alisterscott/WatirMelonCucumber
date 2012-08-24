c469b51f765dd3df937d8fc0db681c107a96bf75
Matthew Johnson <mjohnson@blackboard.com>
2012-08-23 23:55:06 -0400

    add bing unit conversion; fix libs to deal with updated search engine screens; bundle update to get latest selenium-webdriver; switch to ncr urls so results remain in english for travis-ci agents

--- lib/watir-page-helper/pages/google/results_page.rb
+++ lib/watir-page-helper/pages/google/results_page.rb
@@ -1,4 +1,5 @@
 require 'watir-page-helper'
+require 'rspec/expectations'
 
 module WatirPageHelper::Google
   module ResultsPage
@@ -8,12 +9,19 @@ module WatirPageHelper::Google
     expected_element :div, :id => "resultStats"
 
     div :results, :id => "resultStats"
-    h2 :conversion_result, :class => "r"
 
     def number_search_results
       result = /[\s\D]*([\d,]+) results \(\d+\.\d+ seconds\)/.match(results)
       raise "Could not determine number of search results from: '#{results}'" if not result
       result.captures[0].gsub(",","").to_i
+    end
+
+    def conversion_result
+      lhs_d = @browser.text_field(:id => "ucw_lhs_d").value
+      lhs_u = @browser.select_list(:id => "ucw_lhs_u").value
+      rhs_d = @browser.text_field(:id => "ucw_rhs_d").value
+      rhs_u = @browser.select_list(:id => "ucw_rhs_u").value
+      return lhs_d + " " + lhs_u + " = " + rhs_d + " " + rhs_u
     end
   end
 end
